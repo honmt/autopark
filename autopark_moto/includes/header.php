57a7eed64ceb8913ef00876886fce61279c64c31
honmt <145716033+honmt@users.noreply.github.com>
2025-12-05 13:46:12 +0300

    Add files via upload

---
+++ autopark_moto/includes/header.php
@@ -0,0 +1,36 @@
+<?php if (!isset($_SESSION)) session_start(); ?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>AUTOPARK MOTO</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+
+<header class="main-header">
+
+    <div class="header-center">
+
+        <!-- НАЗВАНИЕ -->
+        <div class="header-title">
+            AUTOPARK <span>MOTO</span>
+        </div>
+
+        <!-- МЕНЮ + КНОПКА ВОЙТИ -->
+        <nav class="header-nav">
+        <a href="index.php">Главная</a>
+        <a href="park.php">Автопарк</a>
+        <a href="maintenance.php">График ТО</a>
+        <a href="about.php">О системе</a>
+
+     <?php if (!empty($_SESSION['user'])): ?>
+        <a href="auth/logout.php" class="btn-logout">Выход</a>
+      <?php else: ?>
+        <a href="auth/login.php" class="btn-login">Войти</a>
+     <?php endif; ?>
+    </nav>
+
+    </div>
+
+</header>
