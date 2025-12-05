690d5361dcffb362ae149994177e08086467abf1
honmt <145716033+honmt@users.noreply.github.com>
2025-12-05 13:11:47 +0300

    Delete autopark_moto directory

--- autopark_moto/includes/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="main-footer">
-    <div class="footer-content">
-        <p>AUTOPARK MOTO © 2025. Все права защищены.</p>
-        <p>Система управления мотопарком предприятия</p>
-    </div>
-</footer>
-
-</body>
-</html>
